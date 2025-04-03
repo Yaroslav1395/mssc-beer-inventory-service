53c2a8cb1bd0f63bee8263a7fea2c64deca3cc0b
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-04-03 15:07:33 +0600

    настройка doker compose

--- src/test/java/sakhno/sfg/beer/inventory/service/BeerInventoryEntityApplicationTests.java
+++ src/test/java/sakhno/sfg/beer/inventory/service/BeerInventoryEntityApplicationTests.java
@@ -2,8 +2,10 @@ package sakhno.sfg.beer.inventory.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:application.properties", "classpath:bootstrap.properties"})
 public class BeerInventoryEntityApplicationTests {
 
     @Test
